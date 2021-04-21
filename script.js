bd965193ab27c70dfb1fe8d18c8c75150cd072f6
rodobedrossian <71854709+rodobedrossian@users.noreply.github.com>
2021-04-21 19:27:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function saludar() {
+    alert("Formulario enviado");
+}
+
+document.querySelector('#preguntaForm').innerHTML = "Hola mundo"
+
+let probandoJavascript = document.querySelector('#probandoJavascript');
+probandoJavascript.innerHTML = "A ver qué pasa?"
